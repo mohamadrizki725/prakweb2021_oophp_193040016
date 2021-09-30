e51746d8f9346f3a5338a4d06595aa82e9c7e77b
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-09-30 20:46:59 +0700

    MVC - Controllers

---
+++ pertemuan4/phpmvc/app/controllers/About.php
@@ -0,0 +1,21 @@
+<?php 
+
+class About {
+	public function index($nama = 'Rizki', $pekerjaan = 'Mahasiswa')
+	{
+		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+
+
+	public function page()
+	{
+		echo 'About/page'; 
+	}
+
+
+
+
+}
+
+
+ ?>
---
+++ pertemuan4/phpmvc/app/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+class Home {
+
+	public function index()
+	{
+		$this->view('home/index');
+	}
+}
+
+
+ ?>
